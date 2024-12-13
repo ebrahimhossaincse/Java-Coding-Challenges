501af07e5139e0b0067aa1d469a155fb5be82a75
ebrahimhossaincse <mdebrahimhossain.me@gmail.com>
2024-12-13 04:00:16 -0500

    add a problem

---
+++ src/main/java/me/hossain/ebrahim/Addition_of_Two_Numbers.java
@@ -0,0 +1,13 @@
+package me.hossain.ebrahim;
+
+public class Addition_of_Two_Numbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 5, num2 = 10, sum;
+
+        sum = num1 + num2;
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " = " + sum);
+    }
+}
